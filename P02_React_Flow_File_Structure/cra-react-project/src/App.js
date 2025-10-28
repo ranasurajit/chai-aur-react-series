c5681e06051522a6073e54045c34f1f3aad9c1a9
surajit.rana <surajit1602@gmail.com>
2025-10-28 21:44:32 +0530

    feat[p02][react-flow-folder-structure]: understanding react flow and folder structure

---
+++ P02_React_Flow_File_Structure/cra-react-project/src/App.js
@@ -0,0 +1,12 @@
+import Chai from './Chai';
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to Chai ☕️ aur React ⚛️ series - Using 'create-react-app'</h1>
+      <Chai />
+    </>
+  );
+}
+
+export default App;
---
+++ P02_React_Flow_File_Structure/vite-react-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from './Chai';
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to Chai ☕️ aur React ⚛️ series - Using 'Vite'</h1>
+      <Chai />
+    </>
+  );
+}
+
+export default App;
